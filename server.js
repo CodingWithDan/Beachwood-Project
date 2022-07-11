33a78221bee323308721ca88d900f2b1027b6ddb
Daniel <codingwdan@gmail.com>
2022-07-10 23:52:38 -0700

    added index and folder structure outline

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('employee_info')
     })
 
+ 
+app.set('view engine', 'ejs')
+app.use(express.static('public')) 
+app.use(express.urlencoded({extended:true})) 
+app.use(express.json()) 
+app.use(cors())
+
     
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port`)
 })
